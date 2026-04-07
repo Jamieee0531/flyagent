48b072e25b6759c3aa8c59709bfe252f549792c9
Jamieee0531 <e1554466@u.nus.edu>
2026-04-07 13:18:24 +0800

    feat: register browser_search tool, remove duffel/liteapi references

--- backend/src/tools/builtins/__init__.py
+++ backend/src/tools/builtins/__init__.py
@@ -1,6 +1,5 @@
+from .browser_search_tool import browser_search_tool
 from .clarification_tool import ask_clarification_tool
-from .duffel_tool import duffel_flight_search_tool
-from .liteapi_tool import liteapi_hotel_search_tool
 from .present_file_tool import present_file_tool
 from .setup_agent_tool import setup_agent
 from .task_tool import task_tool
@@ -12,6 +11,5 @@ __all__ = [
     "ask_clarification_tool",
     "view_image_tool",
     "task_tool",
-    "duffel_flight_search_tool",
-    "liteapi_hotel_search_tool",
+    "browser_search_tool",
 ]
